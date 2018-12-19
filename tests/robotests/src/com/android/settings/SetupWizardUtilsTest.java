android/platform_packages_apps_settings

Schematize Setup Wizard System Properties
Properties accessed across partitions are now schematized and will
become APIs to make explicit interfaces among partitions.

Bug: 117924132
Test: mma -j
Change-Id: I53b16e6d784071a04fe3fe1a9aaac8d5be91077b

tests/robotests/src/com/android/settings/SetupWizardUtilsTest.java
@@ -20,7 +20,7 @@ import static com.android.settings.testutils.ResIdSubject.assertResId;
 import static com.google.common.truth.Truth.assertThat;
 
 import android.content.Intent;
-import android.os.SystemProperties;
+import android.sysprop.SetupWizardProperties;
 
 import com.android.settings.testutils.SettingsRobolectricTestRunner;
 import com.android.setupwizardlib.util.WizardManagerHelper;
@@ -47,8 +47,7 @@ public class SetupWizardUtilsTest {
 
     @Test
     public void testGetTheme_withIntentExtra_shouldReturnExtraTheme() {
-        SystemProperties.set(SetupWizardUtils.SYSTEM_PROP_SETUPWIZARD_THEME,
-                WizardManagerHelper.THEME_GLIF);
+        SetupWizardProperties.theme(WizardManagerHelper.THEME_GLIF);
         Intent intent = new Intent();
         intent.putExtra(WizardManagerHelper.EXTRA_THEME, WizardManagerHelper.THEME_GLIF_V2);
 
@@ -57,8 +56,7 @@ public class SetupWizardUtilsTest {
 
     @Test
     public void testGetTheme_withEmptyIntent_shouldReturnSystemProperty() {
-        SystemProperties.set(SetupWizardUtils.SYSTEM_PROP_SETUPWIZARD_THEME,
-                WizardManagerHelper.THEME_GLIF_V2_LIGHT);
+        SetupWizardProperties.theme(WizardManagerHelper.THEME_GLIF_V2_LIGHT);
         Intent intent = new Intent();
 
         assertResId(SetupWizardUtils.getTheme(intent)).isEqualTo(R.style.GlifV2Theme_Light);
@@ -66,8 +64,7 @@ public class SetupWizardUtilsTest {
 
     @Test
     public void testGetTheme_glifV3Light_shouldReturnThemeResource() {
-        SystemProperties.set(SetupWizardUtils.SYSTEM_PROP_SETUPWIZARD_THEME,
-                WizardManagerHelper.THEME_GLIF_V3_LIGHT);
+        SetupWizardProperties.theme(WizardManagerHelper.THEME_GLIF_V3_LIGHT);
         Intent intent = new Intent();
 
         assertResId(SetupWizardUtils.getTheme(intent)).isEqualTo(R.style.GlifV3Theme_Light);

exthmui-aosp/android_packages_apps_Settings

(cherry picked from commit d328f613cbe55ba935f23cef081a0f1e1cc40977)
Former-commit-id: 4b831506871dae864c7f8b5def81c80eef2f191b

tests/robotests/src/com/android/settings/SetupWizardUtilsTest.java
@@ -19,7 +19,7 @@ package com.android.settings;
 import static com.google.common.truth.Truth.assertThat;
 
 import android.content.Intent;
-import android.os.SystemProperties;
+import android.sysprop.SetupWizardProperties;
 
 import com.google.android.setupcompat.util.WizardManagerHelper;
 import com.google.android.setupdesign.util.ThemeHelper;
@@ -47,8 +47,7 @@ public class SetupWizardUtilsTest {
 
     @Test
     public void testGetTheme_withIntentExtra_shouldReturnExtraTheme() {
-        SystemProperties.set(SetupWizardUtils.SYSTEM_PROP_SETUPWIZARD_THEME,
-            ThemeHelper.THEME_GLIF);
+        SetupWizardProperties.theme(ThemeHelper.THEME_GLIF);
         Intent intent = new Intent();
         intent.putExtra(WizardManagerHelper.EXTRA_THEME, ThemeHelper.THEME_GLIF_V2);
 
@@ -57,8 +56,7 @@ public class SetupWizardUtilsTest {
 
     @Test
     public void testGetTheme_withEmptyIntent_shouldReturnSystemProperty() {
-        SystemProperties.set(SetupWizardUtils.SYSTEM_PROP_SETUPWIZARD_THEME,
-            ThemeHelper.THEME_GLIF_V2_LIGHT);
+        SetupWizardProperties.theme(ThemeHelper.THEME_GLIF_V2_LIGHT);
         Intent intent = new Intent();
 
         assertThat(SetupWizardUtils.getTheme(intent)).isEqualTo(R.style.GlifV2Theme_Light);
@@ -66,8 +64,7 @@ public class SetupWizardUtilsTest {
 
     @Test
     public void testGetTheme_glifV3Light_shouldReturnThemeResource() {
-        SystemProperties.set(SetupWizardUtils.SYSTEM_PROP_SETUPWIZARD_THEME,
-            ThemeHelper.THEME_GLIF_V3_LIGHT);
+        SetupWizardProperties.theme(ThemeHelper.THEME_GLIF_V3_LIGHT);
         Intent intent = new Intent();
 
         assertThat(SetupWizardUtils.getTheme(intent)).isEqualTo(R.style.GlifV3Theme_Light);